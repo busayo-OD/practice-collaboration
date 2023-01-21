ef6e87edddc50f9914f4c76c09265e6d3d86e0fa
Olatunji Dada <83545176+conaldes20@users.noreply.github.com>
2023-01-21 13:13:59 +0100

    Adding getter and setter

--- src/main/java/com/example/demo5/model/Student.java
+++ src/main/java/com/example/demo5/model/Student.java
@@ -3,4 +3,15 @@ package com.example.demo5.model;
 public class Student {
 
     private Long id;
+
+    public Student(){
+
+    }
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
 }
